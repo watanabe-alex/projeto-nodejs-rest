27b967a76f34d1704d0d780f5c2bc5e19d5c252f
Alex Watanabe <aowatanabe@gmail.com>
2020-04-06 10:33:41 -0300

    separando reponsabilidades de configuração do servidor, rotas e subir servidor

---
+++ config/customExpress.js
@@ -0,0 +1,17 @@
+// responsabilidade é configura tudo pra gente
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+
+module.exports = () => {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
+    consign()
+        .include('controllers')
+        .into(app);
+    
+    return app;
+}
