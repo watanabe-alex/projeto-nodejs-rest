504a5702f91d48a67c8c070d6757216e414a2e38
Alex Watanabe <aowatanabe@gmail.com>
2020-04-07 11:35:46 -0300

    validação de dados e tratamento de erros

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -7,7 +7,6 @@ module.exports = app => {
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
-        Atendimento.adiciona(atendimento);
-        res.send('post atendimento')
+        Atendimento.adiciona(atendimento, res);
     });
 }
