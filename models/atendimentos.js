a9d60b68d61b4d84696ee50f285bd22d52f4aa1e
Alex Watanabe <aowatanabe@gmail.com>
2020-04-07 15:24:44 -0300

    finanlizando crud

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -37,11 +37,64 @@ class Atendimento {
                 if (erro) {
                     res.status(400).json(erro);
                 } else {
-                    res.status(201).json(resultados);
+                    res.status(201).json(atendimento);
                 }
             });
         };
 
+    };
+
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos';
+
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(resultados);
+            }
+        });
+    };
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados[0];
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json(atendimento);
+            }
+        });
+    };
+
+    altera(id, valores, res) {
+        
+        if(valores.data){
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD hh:mm:ss');
+        };
+        
+        const sql = 'UPDATE Atendimentos SET ? WHERE id=?';
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if (erro) {
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({ ...valores, id });
+            }
+        });
+    };
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?';
+
+        conexao.query(sql, id, (erro, resultados) => {
+            if (erro){
+                res.status(400).json(erro);
+            } else {
+                res.status(200).json({ id });
+            }
+        });
     };
 
 }
